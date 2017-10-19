6a3730c219a41435377871ac0c6011fff4837027
ingnacho <ignacio.ingold@hotmail.com>
2017-10-19 17:04:30 -0300

    merge
    mergeo mi projecto con el de mi compañero

--- Assets/Editor/QuestCreatorWindow.cs
+++ Assets/Editor/QuestCreatorWindow.cs
@@ -10,7 +10,18 @@ public class QuestCreatorWindow : EditorWindow {
 
     int _questIDInt;
     string _questID;
-	
+    int CharacterMaxLvl;
+    int CharacterMinLvl;
+    characterclass clase;
+
+
+    public enum characterclass
+    {
+        paladin,
+        warrior,
+        wizard
+    }
+
     [MenuItem("Custom Windows/Quest Creator Window")]
     static void createWindow()
     {
@@ -21,6 +32,23 @@ public class QuestCreatorWindow : EditorWindow {
     {
         GUILayout.Label("Quest creator window", EditorStyles.boldLabel);
         EditorGUILayout.Space();
+
+        clase = (characterclass)EditorGUILayout.EnumPopup("clase", clase); // pregunto la clase que puede llevar a cabo la quest
+        CharacterMinLvl = EditorGUILayout.IntField("nivel minimo para la quest", CharacterMinLvl); //pido nivel minimo para la mision
+        if (CharacterMinLvl < 1)
+        {
+            EditorGUILayout.HelpBox("el nivel no puede ser menor a 1", MessageType.Info);
+        }
+        CharacterMaxLvl = EditorGUILayout.IntField("nivel maximo para la quest", CharacterMaxLvl);//pido nivel maximo para la mision
+        if (CharacterMaxLvl > 100)
+        {
+            EditorGUILayout.HelpBox("el nivel no puede pasarse de 100", MessageType.Info);
+        }
+
+        if(CharacterMaxLvl-CharacterMinLvl>25)
+        {
+            EditorGUILayout.HelpBox("no se recomienda que el rango sea muy extenso", MessageType.None);
+        }
 
         //var aa = _allQuests.Keys;
 
@@ -34,5 +62,9 @@ public class QuestCreatorWindow : EditorWindow {
         {
             ((QuestLogWindow)GetWindow(typeof(QuestLogWindow))).Show();
         }
+
+        EditorGUILayout.BeginHorizontal("Button");
+        GUILayout.Label("create");
+        EditorGUILayout.EndHorizontal();
     }
 }
