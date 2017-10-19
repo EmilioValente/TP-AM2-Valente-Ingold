f470d495d99611c2badb3922d085f51d87f92a25
ingnacho <ignacio.ingold@hotmail.com>
2017-10-19 16:22:09 -0300

    lvl quest
    creo la ventana y pregunto el nivel del personaje que va a hacer la quest

---
+++ Assets/Editor/quest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class quest : EditorWindow
+{
+    int CharacterLvl;
+
+    [MenuItem("create/ quest")]
+    static void Window()
+    {
+        ((quest)GetWindow(typeof(quest))).Show();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("quest creator", EditorStyles.boldLabel);
+
+        CharacterLvl = EditorGUILayout.IntField("nivel", CharacterLvl);
+    }
+}
